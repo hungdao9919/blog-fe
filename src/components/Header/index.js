bdd9b575ba639ca664b8fb3a29268762c4d8f5c9
hungdao9919 <hungdao9919@gmail.com>
2023-03-08 23:31:25 +0700

    Create project layout

---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import styles from './Header.module.scss'
+function Header(){
+     
+    return <h1 className={styles.ga}>Header</h1>
+}
+export default Header;
---
+++ src/pages/Posts/index.js
@@ -0,0 +1,4 @@
+function PostDetail(){
+    return <h1>PostDetail</h1>
+}
+export default PostDetail;
