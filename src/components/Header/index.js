6191149360e857ea27b21c82affb5b05836a1f2f
hungdao9919 <hungdao9919@gmail.com>
2023-03-14 01:34:01 +0700

    Logo component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import styles from './Header.module.scss'
 import Button from '../Button';
+import Logo from '../Logo';
+import Navigation from '../Navigation';
 function Header(){
      
     return <div className={styles.header_wrapper}>
         <div className={styles.header_container}>
-            <div className={styles.logo_container}>Logo</div>
-            <div className={styles.navigation_container}>Navigation</div>
+            <div className={styles.logo_container}><Logo/></div>
+            <div className={styles.navigation_container}><Navigation/></div>
             <div className={styles.actions_container}> 
             <Button   primary  to='/profile'>Profile</Button> 
             </div>
