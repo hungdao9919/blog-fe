e7a939dd50d65283e4a701aa12ac5f551d347005
hungdao9919 <hungdao9919@gmail.com>
2023-03-27 00:42:42 +0700

    Create Post Component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,12 +1,13 @@
 import styles from './Post.module.scss'
 
-function Post({title,postcontent,datecreated}){
+function Post({title,postcontent,datecreated,username}){
     return <div className={styles.container}>
+        {console.log('Render Post')}
         <p className={styles.title}>{title}</p>
         <p className={styles.short_desc}>{postcontent}</p>
         <div className={styles.details}>
             <p className={styles.date_created}>{datecreated}</p>
-            <p className={styles.author}>Tac gia Hung</p>
+            <p className={styles.author}>{username}</p>
         </div>
     </div>
 }
