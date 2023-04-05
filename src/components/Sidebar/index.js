be2c0e56db7ce75427b662ed4deb6b5739644db2
hungdao9919 <hungdao9919@gmail.com>
2023-04-05 15:00:03 +0700

    comment sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,18 +6,18 @@ import styles from './Sidebar.module.scss'
   function Sidebar (){
     const [posts, setPosts] = useState([]);
   
-    useEffect(  () => {
-        async function getPosts (){
-            const postsResult = await getPublicPosts()
-            setPosts(postsResult) 
-        }
-        getPosts()
-    },[])
-    return  <div className={styles.container}>
-                {console.log('Render Sidebar')}
-                <p>Danh sách bài viết </p>
-                {posts.length>0 && posts.map((post,index)=><Post key={index} id={post.id} title={post.title} dateModify={post.datemodify} postcontent={post.postcontent} datecreated={post.datecreated} username={post.username} />)}
-            </div> 
+    // useEffect(  () => {
+    //     async function getPosts (){
+    //         const postsResult = await getPublicPosts()
+    //         setPosts(postsResult) 
+    //     }
+    //     getPosts()
+    // },[])
+    // return  <div className={styles.container}>
+    //             {console.log('Render Sidebar')}
+    //             <p>Danh sách bài viết </p>
+    //             {posts.length>0 && posts.map((post,index)=><Post key={index} id={post.id} title={post.title} dateModify={post.datemodify} postcontent={post.postcontent} datecreated={post.datecreated} username={post.username} />)}
+    //         </div> 
 
 }
 export default Sidebar;
