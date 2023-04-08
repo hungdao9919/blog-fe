6371a0e7a69022a485a8f3de0803c3b5f37cc3d0
hungdao9919 <hungdao9919@gmail.com>
2023-04-08 23:22:37 +0700

    Change profile Image

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,26 @@
 import styles from './Profile.module.scss'
-import {  useContext } from 'react';
-import { GlobalContext } from '../../context/GlobalContext'; 
-import {hostAPI} from '../../services/configs'   
+import {  useContext,useState } from 'react';
+import { GlobalContext } from '../../context/GlobalContext';   
+import uploadFile from '../../services/uploadFile'; 
+import updateUser from '../../services/updateUser'
 function Profile(){
     const globalContext = useContext(GlobalContext) 
     const profileInfo = globalContext.profileInfo   
-     
+    const [profileImage, setProfileImage] = useState()  
+    const [showSubmit, setShowSubmit] = useState(false)  
+    
+    const handleChangeImageProfile = async (e)=>{ 
+        e.preventDefault(); 
+
+        const uploadResult = await uploadFile(profileImage)  
+        const updateResult = await updateUser({'profileImage':uploadResult})
+        console.log(uploadResult)
+        console.log(updateResult)
+
+    }  
+    const handleSelectUpload = ()=>{
+        setShowSubmit(true)
+    }  
     const result = !profileInfo ? <h2>Vui lòng đăng nhập</h2> : <div className={styles.container} >
         {console.log('Profile render')}
         <h2>{profileInfo._id}</h2>
@@ -15,7 +30,14 @@ function Profile(){
         <h2>{JSON.stringify(profileInfo.roles)}</h2>
         <h2>{profileInfo.email}</h2>
         <h2>{profileInfo.joindate}</h2>
-        <img src={`${profileInfo.profileImage}`}/>
+        <img width='200px' height='200px' src={`${profileInfo.profileImage}`}/>
+        <form onSubmit={handleChangeImageProfile} action="action_page.php">
+        <label htmlFor="profileImage"><b>Change profile image</b></label>
+
+        <input onClick={handleSelectUpload} onChange={(e)=>{setProfileImage(e.target.files[0]) }} type="file" id="profileImage" name="profileImage" accept="image/*" required/>
+
+        {showSubmit && <input type='submit' />}
+        </form>
         <div className={styles.post_list}>
             {}
         </div>
