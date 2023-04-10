c3e883189ab01095950507887965db62fbfccb3a
hungdao9919 <hungdao9919@gmail.com>
2023-04-10 14:24:54 +0700

    Username and username detail css

--- src/components/UserDetail/index.js
+++ src/components/UserDetail/index.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import styles from './UserDetail.module.scss'        
 import getUserDetail from '../../services/getUserdetail';
+import getDateTimeFromTimeStamp from '../../services/getDateTimeFromTimeStamp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';  
+import { faCalendar, faEnvelope, faUser } from '@fortawesome/free-regular-svg-icons';
 function UserDetail ({username}){ 
     const [userDetail, setUserDetail] = useState({})
     useEffect(()=>{
@@ -16,18 +19,34 @@ function UserDetail ({username}){
         setUserDetail(data)
     } 
     
+    const newDateTime = getDateTimeFromTimeStamp(userDetail.createdAt)  
     return <div className={styles.user_detail}>
-        {console.log('render-user detail')}
-        <p>{userDetail.username}</p>
-        <p>{userDetail.email}</p>
-        <p>{userDetail.lastname}</p>
-        <p>{userDetail.firstname}</p>
-        <p>{userDetail.username}</p>
-        <p> {userDetail.joindate}</p>
-        <p> {userDetail.profileImage}</p>
-        <p> {JSON.stringify(userDetail.roles)}</p>
-        <p> {userDetail._id}</p>
-       
+            {console.log('render-user detail')}
+           
+            <div className={styles.profile_image}>
+                <img className={styles.profile_image_img} src={userDetail.profileImage}/> 
+                <div className={styles.background_profile_image}> 
+                    <img src='https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80'/>      
+                </div>
+            </div> 
+            <p  className={styles.fullname}>{userDetail.firstname} {userDetail.lastname}</p>   
+            
+            <div>
+            <div className={styles.username_container}>  
+                <FontAwesomeIcon className={styles.user_icon} icon={faUser}/>  
+            <p  className={styles.username}> {userDetail.username}</p>
+            </div>
+            </div>
+            <div className={styles.username_email_container}>
+            <FontAwesomeIcon className={styles.email_icon} icon={faEnvelope}/>   
+            <p  className={styles.email}>{userDetail.email}</p>  
+            </div>
+             
+            { userDetail.createdAt&&  <div className={styles.profile_statics}>
+                <FontAwesomeIcon className={styles.static_icon} icon={faCalendar}/>   
+
+                <p>{newDateTime.hour}:{newDateTime.minute}:{newDateTime.second} {newDateTime.day}/{newDateTime.month}/{newDateTime.year}</p>  
+            </div> }
        
         </div> 
      
--- src/components/Username/index.js
+++ src/components/Username/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import styles from './Username.module.scss'  
 import UserDetail from '../UserDetail'      
-function Username ({username,...props}){ 
+function Username ({username}){ 
     const [show,setShow] = useState(false)
      
     const handleOnClickUsername=(e)=>{
@@ -9,13 +9,15 @@ function Username ({username,...props}){
         setShow(!show)
         
     }
-     const handleOnMouseOut =()=>{
+    const handleOnMouseOut =()=>{
         setShow(false)
-     }
+    }
     
-    return <div {...props} onClick={handleOnClickUsername} onMouseOut={handleOnMouseOut}>
-        <p  className={styles.username}>{username}</p>
+    return <div  onMouseOver={handleOnClickUsername} onMouseOut={handleOnMouseOut}>
+      <div className={styles.username}>
+        <p>{username}</p>
         {show && <UserDetail  username={username}/>}
+      </div>
         </div> 
      
     
