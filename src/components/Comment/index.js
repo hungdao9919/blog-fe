93bf732194eb5b9e090613a92e9e866beb51a40a
hungdao9919 <hungdao9919@gmail.com>
2023-04-11 10:09:34 +0700

    Comment css

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,20 +1,23 @@
 import styles from './Comment.module.scss'  
-import { useContext, useState,useEffect } from 'react'; 
+import { useContext, useState,useEffect,useRef } from 'react'; 
 import createComment from '../../services/createComment'; 
 import CommentDetails from '../CommentDetails';
 import getPublicComment from '../../services/getPublicComments'; 
 import {GlobalContext} from '../../context/GlobalContext' 
 function Comment(){    
+    const ref = useRef()
+
     const {post,setPost} = useContext(GlobalContext)      
     const [comment, setComment] =useState('')
-    const globalContext = useContext(GlobalContext) 
+    const globalContext = useContext(GlobalContext)   
     const handleCreateComment = async (e)=>{
         e.preventDefault(); 
         const createCommentResult =  await createComment(post._id,comment)   
         const newComment ={...createCommentResult.data,'profileImage':globalContext.profileInfo.profileImage,'username':globalContext.profileInfo.username}  
         let sortedComments = post?.comments?.length > 0 ? post.comments.unshift(newComment) : post.comments =[newComment]
         setPost(prev=>({'comments':(sortedComments),...prev})) 
-        setComment('')
+        setComment('') 
+
     }          
     useEffect(()=>{
         async function getComments (){  
@@ -35,23 +38,33 @@ function Comment(){
         getComments()    
         
         
-},[post._id])      
+},[post._id])    
+    const handleOnchangComment = (e)=>{
+        setComment(e.target.value) 
+        e.target.style.height = '0'
+        e.target.style.height = `${e.target.scrollHeight}px` 
+    }
+    
     return (<div className={styles.wrapper}> 
-        {console.log('Render comment')} 
-        {console.log(post._id)} 
+        {console.log('Render comment')}  
         { globalContext.isLogged &&
             <form onSubmit={handleCreateComment}>
-            <div className={styles.post_editor_container}>
-                <label htmlFor="comment"><b>Bình luận về bài viết này</b></label>
-                <textarea rows="4" cols="50" onChange={(e)=>setComment(e.target.value)} value={comment}  type="text" placeholder="Enter content" name="comment" required/>
-                <button type="submit">Post</button>                 
+            <div className={styles.comment_editor_container}>
+                {globalContext?.profileInfo && <label className={styles.image_label} htmlFor="comment"><b><img src={globalContext.profileInfo.profileImage} /></b></label>}
+                <div className={styles.textarea_container}>
+                    <textarea ref={ref} rows='1' cols="50" onChange={(e)=>handleOnchangComment(e)} value={comment}  type="text" placeholder="Leave your comment for this post" id='comment' name="comment" required/>
+                    <div className={styles.comments_actions_container}>
+                        <button type="submit">Post</button>       
+                        {/* scale thêm chức năng upload hình ảnh,... */}
+                    </div>
+                </div>
+                           
             </div>
         </form> 
-        }
-        
+        } 
         {(post?.comments?.length > 0 ? post.comments.map((comment,index)=>{  
             return  <CommentDetails key={index} profileImage={comment.profileImage} username ={comment.username}  commentcontent ={comment.commentcontent}  createdAt={comment.createdAt}  userid ={comment.userid}  _id ={comment._id} />
-        }):<div>Không có bình luận nào về bài viết này</div>)}
+        }):<div>No comment yet</div>)}
     </div>)
                 
 }
