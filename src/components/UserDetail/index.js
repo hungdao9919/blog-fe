71ec41ce26ae3a2dcb0947685eb0abc10f1768c3
hungdao9919 <hungdao9919@gmail.com>
2023-04-11 10:09:56 +0700

    Fix bugs and update css

--- src/components/UserDetail/index.js
+++ src/components/UserDetail/index.js
@@ -21,27 +21,27 @@ function UserDetail ({username}){
     
     const newDateTime = getDateTimeFromTimeStamp(userDetail.createdAt)  
     return <div className={styles.user_detail}>
-            {console.log('render-user detail')}
-           
+            {console.log('render-user detail')} 
+            
             <div className={styles.profile_image}>
                 <img className={styles.profile_image_img} src={userDetail.profileImage}/> 
                 <div className={styles.background_profile_image}> 
                     <img src='https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80'/>      
                 </div>
             </div> 
+
             <p  className={styles.fullname}>{userDetail.firstname} {userDetail.lastname}</p>   
-            
-            <div>
+             
             <div className={styles.username_container}>  
                 <FontAwesomeIcon className={styles.user_icon} icon={faUser}/>  
-            <p  className={styles.username}> {userDetail.username}</p>
-            </div>
-            </div>
+                <p  className={styles.username}> {userDetail.username}</p>
+            </div> 
+
             <div className={styles.username_email_container}>
-            <FontAwesomeIcon className={styles.email_icon} icon={faEnvelope}/>   
-            <p  className={styles.email}>{userDetail.email}</p>  
-            </div>
-             
+                <FontAwesomeIcon className={styles.email_icon} icon={faEnvelope}/>   
+                <p  className={styles.email}>{userDetail.email}</p>  
+            </div> 
+
             { userDetail.createdAt&&  <div className={styles.profile_statics}>
                 <FontAwesomeIcon className={styles.static_icon} icon={faCalendar}/>   
 
